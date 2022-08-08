a0b64d314d7a13f77681fd527ef977a6b73459ac
mocomocoafro <engineer@creative1031.com>
2022-08-08 17:23:58 +0900

    style setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bentleyLight: ['Bentley-Light', 'Noto Sans JP', 'sans-serif'],
+        bentleyRegular: ['Bentley-Regular', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        bentleyTheme: {
+          primary: '#335B4C',
+          secondary: '#757575',
+          accent: '#94C11D',
+          neutral: '#5B5B5B',
+          'base-100': '#ffffff',
+          'base-200': '#F5F7F6',
+          'base-300': '#E6ECE9',
+          'base-content': '#333333',
+
+          '--rounded-btn': '0',
+        },
+      },
+    ],
+  },
 }
