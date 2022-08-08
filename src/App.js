0605ea4403dd07f9f50933871fd0ca43760ec940
mocomocoafro <engineer@creative1031.com>
2022-08-08 17:59:23 +0900

    fixed custom fonts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto font-bentleyLight">
       <button className="btn btn-primary">Button</button>
       <h1>CONTINENTAL GT</h1>
       <p>ベントレーモーターズジャパン公式 instagram</p>
