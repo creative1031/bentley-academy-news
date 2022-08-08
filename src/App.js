5f63bc2d2e91777298e2a62714e276c3a45a2ba5
mocomocoafro <engineer@creative1031.com>
2022-08-08 17:31:21 +0900

    app.js changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <div className="container mx-auto">App</div>
+  return (
+    <div className="container mx-auto">
+      <button className="btn btn-primary">Button</button>
+      <h1>CONTINENTAL GT</h1>
+      <p>ベントレーモーターズジャパン公式 instagram</p>
+    </div>
+  )
 }
 
 export default App
