32ddcbd718d415ca9e797f996966c5183eafba78
mocomocoafro <engineer@creative1031.com>
2022-09-06 11:18:02 +0900

    fixed pickup

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 const articles = [
   { title: 'Beyond 100', src: 'beyond100', term: 'BEYOND 100' },
   {
-    title: 'Product Knowledge',
+    title: '基礎知識',
     src: 'knowledge',
     term: 'PRODUCT KNOWLEDGE',
   },
@@ -20,15 +20,15 @@ export default function ArticlesList() {
 
   return (
     <div className="py-5 border-t">
-      <h3 className="text-center text-xl mb-5 md: mb-7 xl:mt-5">PICK UP</h3>
-      <ul className="grid md:grid-cols-2 gap-10 lg:grid-cols-3 xl:grid-cols-4">
+      <h3 className="text-center text-xl my-5 md:mb-7 xl:mt-5">PICK UP</h3>
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-5 lg:grid-cols-4 xl:grid-cols-5">
         {articles.map((article) => (
           <div key={article.title}>
             <li
-              className={`bg-${article.src} bg-no-repeat bg-cover bg-center w-full h-40 cursor-pointer `}
+              className={`bg-${article.src} bg-no-repeat bg-cover bg-center w-full h-24 cursor-pointer md:h-24 lg:h-28 xl:h-32`}
               onClick={() => handleClick(article.term)}
             ></li>
-            <p className="text-center mt-2">{article.title}</p>
+            <p className="text-center text-sm my-2">{article.title}</p>
           </div>
         ))}
       </ul>
